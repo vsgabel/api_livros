6d49754060560d8b035bf7142ac86147ddfc36fa
Victor Gabel <victor.gabel@gmail.com>
2022-08-25 00:38:54 -0300

    correção da variavel de ambiente do db

--- config.py
+++ config.py
@@ -13,6 +13,8 @@ class Config:
 class Heroku(Config):
     SSL_REDIRECT = True if os.getenv("DYNO") else False
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://")
 
     @classmethod
     def init_app(cls, app):
